a895380bff30ed44ef4a6122a862a41c79079358
jakravit <jakravit@forza2.local>
2022-08-01 14:37:42 -0500

    test updats

--- test_parallel_02.sh
+++ test_parallel_02.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #make directory
 mkdir -p run_$1
-#cp commandline_eda_01.ipynb run_$1/commandline_eda_01.ipynb
-#cp run_EAP_batch.py run_$1/run_EAP_batch.py
-#cp EAP_tn.py run_$1/EAP_tn.py
-#cp Dmmex_R14B_4.f run_$1/Dmmex_R14B_4.f
-#cp Dmmex_R14B_4.cpython-39-x86_64-linux-gnu.so run_$1/Dmmex_R14B_4.cpython-39-x86_64-linux-gnu.so
-cp {run_EAP_batch.py, EAP_tn.py, Dmmex_R14B_4.f, Dmmex_R14B_4.cpython-39-x86_64-linux-gnu.so} run_$1/
+cp run_batch.py run_$1/run_batch.py
+cp EAP_tn.py run_$1/EAP_tn.py
+cp Dmmex_R14B_4.f run_$1/Dmmex_R14B_4.f
+cp Dmmex_R14B_4.cpython-39-x86_64-linux-gnu.so run_$1/Dmmex_R14B_4.cpython-39-x86_64-linux-gnu.so
+#cp {run_EAP_batch.py, EAP_tn.py, Dmmex_R14B_4.f, Dmmex_R14B_4.cpython-39-x86_64-linux-gnu.so} run_$1/
 cd run_$1
 
 #set environment
@@ -17,4 +16,4 @@ source activate tf2_8
 
 #run code
 echo "Executing run $1 on" `hostname` "in $PWD"
-/swbuild/analytix/tools/miniconda3_220407/envs/tf2_8/bin/python run_EAP_batch.py $1
+/swbuild/analytix/tools/miniconda3_220407/envs/tf2_8/bin/python run_batch.py $1
