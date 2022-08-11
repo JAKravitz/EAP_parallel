996baf1aa24a863a290249a7a82ee9c8e7a21fa5
JAKravitz <jeremy.kravitz@gmail.com>
2022-08-11 09:47:31 -0500

    Update run_batch.py

--- run_batch.py
+++ run_batch.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
                   'Sigma_bb',
                   'bbstar',
                   'VSF',
-                  'psdvol]
+                  'psdvol']
 
 
     # wavelength range and resolution 
